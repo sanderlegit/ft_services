6645371a71760663d13027e66ada36e104210ca9
MrOsteoporosis <sanderverheijen@outlook.com>
2020-12-23 16:41:08 +0100

    skeleton setup.sh /w MetalLB (bless you bictor)

---
+++ setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# ---------- Colors ---------- #
+RED=$'\e[1;31m'
+GREEN=$'\e[1;32m'
+YELLOW=$'\e[1;33m'
+BLUE=$'\e[1;34m'
+MAGENTA=$'\e[1;35m'
+CYAN=$'\e[1;36m'
+END=$'\e[0m'
+
+# ---------- Modular function for starting apps ---------- #
+# $1 = name, $2 = docker-location, $3 = yml-location
+start_app () {
+	printf "$1: "
+	if [ "$4" == "--debug" ]
+	then
+		docker build -t $1 $2 && kubectl apply -f $3
+	else
+		docker build -t $1 $2 > /dev/null 2>>errlog.txt && kubectl apply -f $3 > /dev/null 2>>errlog.txt
+	fi
+    RET=$?
+	if [ $RET -eq 1 ]
+	then
+		echo "[${RED}NO${END}]"
+	else
+		echo "[${GREEN}OK${END}]"
+	fi
+
+# ---------- Setting debug ---------- #
+DEBUG=$""
+if [ $# -eq 1 ]
+then
+    DEBUG="--debug"
+fi
+
+# ---------- Cleanup ---------- #
+#rm -rf ~/.minikube
+#mkdir -p ~/goinfre/.minikube
+#ln -s ~/goinfre/.minikube ~/.minikube
+
+:> errlog.txt
+
+# ---------- Cluster start ---------- #
+minikube start
+
+# ---------- Build and deploy ---------- #
+eval $(minikube docker-env)
+export MINIKUBE_IP=$(minikube ip)
+
+kubectl apply -f ./srcs/MetalLB/config.yaml
