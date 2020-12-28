3374b9fe90f3d8e3048636cb8eecff4343ab3696
MrOsteoporosis <sanderverheijen@outlook.com>
2020-12-28 17:24:45 +0100

    removed unused .sh

--- first_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# MetalLB configuration
-kubectl apply -f ./srcs/MetalLB/namespace.yaml
-kubectl apply -f ./srcs/MetalLB/metallb.yaml
-# On first install only
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
