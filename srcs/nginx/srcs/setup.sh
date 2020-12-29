07ad87114da6f1905499976a5da284ec2e37a39b
MrOsteoporosis <sanderverheijen@outlook.com>
2020-12-29 18:46:07 +0100

    max potaot

--- srcs/nginx/srcs/setup.sh
+++ srcs/nginx/srcs/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # dynamic links to services, clean
-. /tmp/get_external_ips.sh WORDPRESS_SVC wordpress-svc
-. /tmp/get_external_ips.sh PHPMYADMIN_SVC phpmyadmin-svc
-. /tmp/get_external_ips.sh GRAFANA_SVC grafana-svc
+. /tmp/get_external_ip.sh WORDPRESS_SVC wordpress-svc
+. /tmp/get_external_ip.sh PHPMYADMIN_SVC phpmyadmin-svc
+. /tmp/get_external_ip.sh GRAFANA_SVC grafana-svc
 envsubst '${WORDPRESS_SVC} ${PHPMYADMIN_SVC} ${GRAFANA_SVC}' < /tmp/index.html > /www/index.html
 rm /tmp/index.html
 # ssh user
