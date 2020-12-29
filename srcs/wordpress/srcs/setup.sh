612ce629c0670cdb5f837d3986edd4c85cb1ce1c
MrOsteoporosis <sanderverheijen@outlook.com>
2020-12-29 12:39:29 +0100

    wordpress cleanup

--- srcs/wordpress/srcs/setup.sh
+++ srcs/wordpress/srcs/setup.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-#get wp install archive
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-#nginx permissiosn
-chown -R www:www /var/lib/nginx
-chown -R www:www /www
-#find external IP
+# find external IP
 . /tmp/get_external_ip.sh WPSVC_IP wordpress-svc
-#set enviroment supplied by .yaml secrets
+# set enviroment supplied by .yaml secrets for wp-config.php, remove template
 envsubst '${WPSVC_IP} ${DB_NAME} ${DB_USER} ${DB_PASSWORD} ${DB_HOST}' < /tmp/wp-config.php > /www/wp-config.php
-#remove template file
 rm /tmp/wp-config.php
-#set permissions
-chmod +x wp-cli.phar
-#add to path
-mv wp-cli.phar /usr/local/bin/wp
-#run install/test shell
-cd /www
+# set permissions for wp-cli, add to path
+chmod +x /tmp/wp-cli.phar
+mv /tmp/wp-cli.phar /usr/local/bin/wp
+# run install/test shell
 su www -c "/tmp/wpinstall.sh"
-#clean
+# clean
 rm -rf /root/.wp-cli
