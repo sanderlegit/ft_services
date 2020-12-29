23bfe3ec649dea383edc3231c9f419db472b6294
MrOsteoporosis <sanderverheijen@outlook.com>
2020-12-29 18:38:56 +0100

    nginx index fixed dynamic links

--- srcs/nginx/srcs/setup.sh
+++ srcs/nginx/srcs/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-
-. /tmp/get_external-ip-address.sh WORDPRESS_SVC wordpress-svc
-. /tmp/get_external-ip-address.sh PHPMYADMIN_SVC phpmyadmin-svc
-. /tmp/get_external-ip-address.sh GRAFANA_SVC grafana-svc
-
+# dynamic links to services, clean
+. /tmp/get_external_ips.sh WORDPRESS_SVC wordpress-svc
+. /tmp/get_external_ips.sh PHPMYADMIN_SVC phpmyadmin-svc
+. /tmp/get_external_ips.sh GRAFANA_SVC grafana-svc
 envsubst '${WORDPRESS_SVC} ${PHPMYADMIN_SVC} ${GRAFANA_SVC}' < /tmp/index.html > /www/index.html
-
+rm /tmp/index.html
+# ssh user
 adduser --disabled-password ${SSH_USERNAME}
 echo "${SSH_USERNAME}:${SSH_PASSWORD}" | chpasswd
