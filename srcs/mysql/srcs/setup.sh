b5a78ab22c6a1116c555f8a2aade4897da5feebc
MrOsteoporosis <sanderverheijen@outlook.com>
2020-12-29 18:39:00 +0100

    mysql slim sh

--- srcs/mysql/srcs/setup.sh
+++ srcs/mysql/srcs/setup.sh
@@ -1,25 +1,13 @@
+#configure database user
 envsubst '${DB_USER} ${DB_PASSWORD}' < /tmp/my.cnf > /etc/my.cnf
-
 mysql_install_db --user=$DB_USER --ldata=/var/lib/mysql
-
-:> /tmp/sql
 cat /tmp/create_tables.sql >> /tmp/sql
 echo "" >> /tmp/sql
-# allow external connections
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" >> /tmp/sql
 echo "SET PASSWORD FOR '$DB_USER'@'localhost'=PASSWORD('${DB_PASSWORD}') ;" >> /tmp/sql
 echo "GRANT ALL ON *.* TO '$DB_USER'@'127.0.0.1' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;" >> /tmp/sql
 echo "GRANT ALL ON *.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;" >> /tmp/sql
 echo "GRANT ALL ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;" >> /tmp/sql
 echo "FLUSH PRIVILEGES;" >> /tmp/sql
-
+#startup db with config
 /usr/bin/mysqld --console --init_file=/tmp/sql
-
-#echo "\nCREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; \n\
-	#SET PASSWORD FOR '$DB_USER'@'localhost'=PASSWORD('${DB_PASSWORD}') ; \n\
-	#GRANT ALL ON *.* TO '$DB_USER'@'127.0.0.1' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION; \n\
-	#GRANT ALL ON *.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION; \n\
-	#GRANT ALL ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION; \n\
-	#FLUSH PRIVILEGES;" >> /tmp/create_tables.sql
-
-#/usr/bin/mysqld --console --init_file=/tmp/create_tables.sql
