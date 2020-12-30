921926a61a1f42c6546e061bd0d2767ab81d87cc
MrOsteoporosis <sanderverheijen@outlook.com>
2020-12-30 14:05:14 +0100

    nginx more links

--- srcs/nginx/srcs/setup.sh
+++ srcs/nginx/srcs/setup.sh
@@ -4,7 +4,7 @@
 . /tmp/get_external_ip.sh PHPMYADMIN_SVC phpmyadmin-svc
 . /tmp/get_external_ip.sh GRAFANA_SVC grafana-svc
 . /tmp/get_external_ip.sh NGINX_SVC nginx-svc
-envsubst '${WORDPRESS_SVC} ${PHPMYADMIN_SVC} ${GRAFANA_SVC}' < /tmp/index.html > /www/index.html
+envsubst '${WORDPRESS_SVC} ${PHPMYADMIN_SVC} ${NGINX_SVC} ${GRAFANA_SVC}' < /tmp/index.html > /www/index.html
 rm /tmp/index.html
 envsubst '${WORDPRESS_SVC} ${PHPMYADMIN_SVC} ${NGINX_SVC}' < /tmp/default.conf > /etc/nginx/conf.d/default.conf
 rm /tmp/default.conf
